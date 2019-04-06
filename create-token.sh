e28cd7b5614298fa098c08494257d34f59c91e8c
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2019-04-06 10:36:47 +0200

    reduce manual work by adding task to deployer.

---
+++ create-token.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+TOKEN=$1
+
+echo $TOKEN > /home/rockyj/Workspace/2-Personal/91-Go/src/deployer/.dockercreds
--- run.go
+++ run.go
@@ -9,17 +9,37 @@ import (
 	"os/exec"
 )
 
-type Message struct {
+type SessionMessage struct {
+	Token string `json:"token"`
+}
+
+type DeploymentMessage struct {
 	ServiceName string `json:"service-name"`
 	ImagePath   string `json:"image-path"`
 }
 
-func runCmd(t *Message) string {
+func createToken(sm *SessionMessage) string {
+	// app := "./create-token.sh"
+	app := "/opt/deployer/create-token.sh"
+
+	arg1 := sm.Token
+
+	cmd := exec.Command(app, arg1)
+	stdout, err := cmd.Output()
+
+	if err != nil {
+		return string(err.Error())
+	}
+
+	return string(stdout)
+}
+
+func deploy(dm *DeploymentMessage) string {
 	// app := "./restart-service.sh"
 	app := "/opt/deployer/restart-service.sh"
 
-	arg1 := t.ServiceName
-	arg2 := t.ImagePath
+	arg1 := dm.ServiceName
+	arg2 := dm.ImagePath
 
 	cmd := exec.Command(app, arg1, arg2)
 	stdout, err := cmd.Output()
@@ -35,15 +55,30 @@ func HealthHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "OK!")
 }
 
-func MainHandler(w http.ResponseWriter, req *http.Request) {
+func SessionHandler(w http.ResponseWriter, req *http.Request) {
 	if req.Method == "POST" {
+		var sm SessionMessage
 		decoder := json.NewDecoder(req.Body)
-		var t Message
-		err := decoder.Decode(&t)
+		err := decoder.Decode(&sm)
 		if err != nil {
 			http.Error(w, "Invalid request", http.StatusBadRequest)
 		} else {
-			fmt.Fprintf(w, runCmd(&t))
+			fmt.Fprintf(w, createToken(&sm))
+		}
+	} else {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	}
+}
+
+func DeploymentHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method == "POST" {
+		var dm DeploymentMessage
+		decoder := json.NewDecoder(req.Body)
+		err := decoder.Decode(&dm)
+		if err != nil {
+			http.Error(w, "Invalid request", http.StatusBadRequest)
+		} else {
+			fmt.Fprintf(w, deploy(&dm))
 		}
 	} else {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
@@ -51,7 +86,8 @@ func MainHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-	http.Handle("/__deployer/health", httpauth.SimpleBasicAuth(os.Getenv("DEPLOYER_USER"), os.Getenv("DEPLOYER_PASSWORD"))(http.HandlerFunc(HealthHandler)))
-	http.Handle("/__deployer/deploy", httpauth.SimpleBasicAuth(os.Getenv("DEPLOYER_USER"), os.Getenv("DEPLOYER_PASSWORD"))(http.HandlerFunc(MainHandler)))
+	http.Handle("/__deployer/health", 		httpauth.SimpleBasicAuth(os.Getenv("DEPLOYER_USER"), os.Getenv("DEPLOYER_PASSWORD"))(http.HandlerFunc(HealthHandler)))
+	http.Handle("/__deployer/session", 		httpauth.SimpleBasicAuth(os.Getenv("DEPLOYER_USER"), os.Getenv("DEPLOYER_PASSWORD"))(http.HandlerFunc(SessionHandler)))
+	http.Handle("/__deployer/deployment", httpauth.SimpleBasicAuth(os.Getenv("DEPLOYER_USER"), os.Getenv("DEPLOYER_PASSWORD"))(http.HandlerFunc(DeploymentHandler)))
 	http.ListenAndServe(":7000", nil)
 }
